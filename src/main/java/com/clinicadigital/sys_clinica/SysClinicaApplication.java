f02af1a183df7bec28f36e10bbb62a19b1557152
Sebastian Oliveto <s.oliveto93@gmail.com>
2025-01-26 13:34:21 -0300

    first commit

---
+++ src/main/java/com/clinicadigital/sys_clinica/SysClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.clinicadigital.sys_clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysClinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicadigital/sys_clinica/SysClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicadigital.sys_clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
